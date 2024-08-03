794ac8f74def4993ad059a09d9f8658def0276c7
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2024-08-03 18:24:45 +0530

    update day

---
+++ Day 5/p1.java
@@ -0,0 +1,12 @@
+class p1{
+	public static void main(String[] args)
+  {
+	int age = 18;
+	if(age>=18){
+	System.out.println("Your age is 18+ than your drive car");
+	}
+	else{
+	System.out.println("Your age is not 18+ than your not drive car");
+	}	
+   }
+}
---
+++ Day 6/p1.java
@@ -0,0 +1,8 @@
+class p1{	
+	public static void main(String[] args){
+	for(int i=0;i<=3;i++)
+	{
+	System.out.print(i,"*");
+	}
+	}	
+}
