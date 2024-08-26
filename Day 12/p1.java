369332741f51aa846c460ac08e49c3511d85959d
sumitdashrathgupta <sumitdashrathgupta111@gmai.com>
2024-08-26 21:21:07 +0530

    update

---
+++ Day 12/p1.java
@@ -0,0 +1,30 @@
+/*class p1
+{
+	public static void main(String[] arg)
+	{
+	System.out.println(10+20+"Java");
+	System.out.println("java"+10+20);
+	System.out.println(10+"java"+20);
+	System.out.println("java"+2*3);
+	}
+}*/
+
+
+/*class p1 {
+    public static void main(String[] args) {
+        System.out.println(1.0 / 0);
+        System.out.println(0.0 / 0);
+        System.out.println(0 / 0);
+    }
+}*/
+
+
+
+/* class p1 {
+    public static void main(String[] args) {
+        String s = "abc";
+        String b = s.toUpperCase();
+        System.out.println(s);
+	 System.out.println(b);
+    }
+}*/
