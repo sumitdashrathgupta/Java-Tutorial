fb02fe11c87d94220dc4fe19b5c4745e5b9adecc
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2024-08-28 22:07:36 +0530

    update

---
+++ Day 11/p1.java
@@ -0,0 +1,8 @@
+class p1{
+    public static void main(String[] args){
+        // System.out.println("");
+        // System.out.print("");
+        // System.out.println();
+         System.out.print();
+    }
+}
---
+++ Day 12/p1.java
@@ -0,0 +1,14 @@
+class p1{
+	public static void main(String[] args)
+	{
+	int a=99;
+	if(a%3 == 0)
+	{
+	System.out.println("Even number "+a);
+	}
+	else
+	{
+	System.out.println("Odd Number "+a);
+	}
+	}
+}
