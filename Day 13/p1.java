10cb3979c3aef434091b60dc7d14d70887582529
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2024-08-31 18:41:35 +0530

    update

---
+++ Day 13/p1.java
@@ -0,0 +1,18 @@
+class p1{
+	public static void main(String[] args)
+	{
+	int age=100;
+	if(age>18 && age<=39)
+	{
+	System.out.println("Your age is "+ age +" you can drive ");
+	}
+	else if(age<=80 && age>=40 )
+	{
+	System.out.println("To drive some condition on rord. your age is "+age );
+	}
+	else
+	{
+	System.out.println("Your age is "+ age+" you can not drive");
+	}
+	}
+}
