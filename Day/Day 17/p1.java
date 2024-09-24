ee15dbee84360f3fe884f35d44f120db80731098
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2024-09-24 22:57:57 +0530

    update

---
+++ Day/Day 17/p1.java
@@ -0,0 +1,15 @@
+class p1{
+	public static void test()
+	{
+	int i = 20;
+	System.out.println("Start Test");
+	System.out.println(i);
+	System.out.println("Ends Test");
+	}
+	public static void main(String[] args)
+	{
+	System.out.println("Start main");
+	test();
+	System.out.println("Ends Main");
+	}
+}
---
+++ Day/Day 17/p2.java
@@ -0,0 +1,8 @@
+class p2
+{
+	public static void main(String[] args)
+	{
+	
+	}
+
+}
